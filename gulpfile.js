f67eb8773d56438b8a129667e1f76f0acdeb4a09
sohye <sohyek@white64.com>
2024-11-18 09:11:02 -0500

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,35 @@ import gulp from 'gulp';
 import cleanCss from 'gulp-clean-css';
 import dartSass from 'sass'
 import gulpSass from 'gulp-sass'
-import gulpIf from 'gulp-if';
+import gulpif from 'gulp-if';
 import sourcemaps from 'gulp-sourcemaps';
 
 
 // const PRODUCTION = yargs.argv.prod;
 const sass = gulpSass(dartSass)
+const y = yargs();
+const PRODUCTION = y.argv.prod;
 
+const paths = {
+    styles: {
+        src: ['src/assets/scss/bundle.scss', 'src/assets/scss/admin.scss'],
+        dest: 'dist/assets/css'
+    }
+}
  
 
 export const styles = () => {
-    console.log('yars: ', yargs.argv);
-    return gulp.src('src/assets/scss/bundle.scss')
-        .pipe(sourcemaps.init())
-            .pipe(sass().on('error', sass.logError))
-            .pipe(cleanCss({compatibility: 'ie8'}))
-            // using gulfif, only for production
-            // .pipe(gulpif(PRODUCTION, cleanCss({compatibility: 'ie8'})))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('dist/assets/css'));
+    return gulp.src(paths.styles.src)
+        .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulpif(PRODUCTION, cleanCss({compatibility: 'ie8'})))
+        .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
+        .pipe(gulp.dest(paths.styles.dest));
+}
+
+export const watch = () => {
+    console.log('##### IN PRODUCTION #####')
+    gulp.watch('src/assets/scss/**/*.scss', styles);
 }
 
 // export default styles;
--- lib/enqueue-assets.php
+++ lib/enqueue-assets.php
@@ -5,4 +5,9 @@ function pluto_assets() {
 }
 add_action( 'wp_enqueue_scripts', 'pluto_assets' );
 
+function pluto_admin_assets() {
+    wp_enqueue_style( 'pluto-admin-stylesheet', get_template_directory_uri() . '/dist/assets/css/admin.css', array(), '1.0.0', 'all' );
+}
+add_action( 'admin_enqueue_scripts', 'pluto_admin_assets' );
+
 ?>
